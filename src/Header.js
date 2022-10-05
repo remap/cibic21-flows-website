5cb5fdc2a13531178e23e0d3d508cccd5f765690
leo martinez <leohque@gmail.com>
2022-10-04 21:12:38 -0700

    quick setup/google fonts/component skeleton

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Header.css';
+
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <h1>FLOWS</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;

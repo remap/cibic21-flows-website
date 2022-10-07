2607f9732220cd250d7f77253d7de778eaad07de
leo martinez <leohque@gmail.com>
2022-10-06 22:41:54 -0700

    refactored components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header.js'
 import Footer from './components/Footer.js';
-import Viz from './components/Viz.js';
+import Map from './components/Map.js';
 
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
@@ -11,30 +11,10 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2liaWMtbWFwYm94IiwiYSI6ImNremoyd2tieTA1dXoyb
 
 
 function App() {
-
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-118.4452);
-  const [lat, setLat] = useState(34.0689);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-  if (map.current) return; // initialize map only once
-  map.current = new mapboxgl.Map({
-  container: mapContainer.current,
-  style: 'mapbox://styles/cibic-mapbox/cl8265z24000214mooehh9my5',
-  center: [lng, lat],
-  zoom: zoom
-  });
-  });
-
-
   return (
     <div className="App">
       <Header />
-      <div>
-        <div ref={mapContainer} className="map-container" />
-      </div>
+      <Map />
       <Footer />
     </div>
   );

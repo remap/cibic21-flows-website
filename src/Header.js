6615d4f56961493bb42aab95abd9b584aefadde7
leo martinez <leohque@gmail.com>
2022-10-06 20:20:19 -0700

    imported svg icons and header love

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './Header.css';
 
+import globe from './assets/globe.svg';
+import help from './assets/help.svg'
+
 
 function Header() {
   return (
     <header>
-      <div>
-        <h1>FLOWS</h1>
+      <div className="header-row">
+        <h1 className="title">Flows</h1>
+        <img className="icon" src={globe} alt="Language Translate"/>
+      </div>
+      <div className="header-row">
+        <select name="city-selector" id="city-selector">
+          <option value="la" id="flyUCLA">Los Angeles</option>
+          <option value="ba" id="flyBA">Buenos Aires</option>
+        </select>
+        <img className="icon" src={help} alt="More Information" />
       </div>
     </header>
   );

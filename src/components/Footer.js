c711ca55971232877bb6bbb4462c08c17e71d296
leo martinez <leohque@gmail.com>
2022-10-06 20:25:11 -0700

    refactored structure

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/cibic_logo.png';
+import './Footer.css';
+
+
+function Footer() {
+  return (
+    <footer>
+        <img src={logo} />
+    </footer>
+  );
+}
+
+export default Footer;

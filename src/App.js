a8b1eafa377df35744b45acdf566d5ac4ffbba7a
leo martinez <leohque@gmail.com>
2022-10-06 22:08:12 -0700

    added mapbox

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header.js'
 import Footer from './components/Footer.js';
 import Viz from './components/Viz.js';
 
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2liaWMtbWFwYm94IiwiYSI6ImNremoyd2tieTA1dXoyb21xN3E3anZsdmcifQ.ILbkkSjS8PpQkbr_VivhgQ';
+
+
 
 function App() {
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+  if (map.current) return; // initialize map only once
+  map.current = new mapboxgl.Map({
+  container: mapContainer.current,
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [lng, lat],
+  zoom: zoom
+  });
+  });
+
+
   return (
     <div className="App">
       <Header />
-      <Viz />
+      <div>
+        <div ref={mapContainer} className="map-container" />
+      </div>
       <Footer />
     </div>
   );
